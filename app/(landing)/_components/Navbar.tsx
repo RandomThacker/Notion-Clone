3fe4de4d1df7cc193c600d9de7d01fd510bfc612
RandomThacker <randomthacker@gmail.com>
2024-04-17 00:25:59 +0530

    Add: Navbar

---
+++ app/(landing)/_components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useScrollTop } from "@/hooks/useScrollTop";
+import { cn } from "@/lib/utils";
+import { Logo } from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+
+export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
+
+  return (
+    <nav
+      className={cn(
+        "sticky inset-x-0 top-0 z-50 mx-auto flex w-full items-center bg-background p-6 dark:bg-[#1F1F1F]",
+        scrolled && "border-b shadow-sm",
+      )}
+    >
+      <Logo />
+      <div className="flex w-full items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <Spinner />}
+        {!isLoading && !isAuthenticated && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Zotion Free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents"> Enter Zotion </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+};
